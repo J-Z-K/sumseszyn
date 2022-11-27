bf532c302630bd079bd0442a74e97aa01a68735a
Michał Kadzik <Michal.Kadzik@ringieraxelspringer.pl>
2022-11-27 15:08:28 +0100

    wip

--- backend/src/models/question.model.js
+++ backend/src/models/question.model.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const questionSchema = new mongoose.Schema({
   questionContent: String,
   answers: [String],
-  answerCorrect: String
+  answerCorrect: [Number]
 })
 
 const question = new mongoose.model('Questions', questionSchema)
